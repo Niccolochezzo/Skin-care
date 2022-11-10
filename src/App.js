7c1def5d842586c6834e0d904ef99957cfc57cf3
Niccolochezzo <niccolochezzo@gmail.com>
2022-11-11 02:02:18 +0400

    Product/animation added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
 import Slider from "./components/Slider/Slider";
+import Virtual from "./components/Virtual/Virtual";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header />
       <Hero />
       <Slider />
+      <Virtual />
+      <Products />
     </div>
   );
 }
